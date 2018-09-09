332c2ccd499fa6b1fba228180fa9a78c8157d6aa
JulioCVaz <julio.oliveiravaz0@gmail.com>
2018-09-09 13:52:29 -0300

    desenvolvimento inicial site

---
+++ site/gulpfile.js
@@ -0,0 +1,7 @@
+var gulp = require('gulp');
+
+gulp.task('default', function(){
+	//TODO
+});
+
+gulp.start.apply(gulp, ['default']);
---
+++ site/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>som de garagem</title>
+    <!-- bootstrap e css -->
+    <link rel="stylesheet" href="assets/components/dist/css/bootstrap.min.css" type="text/css">
+</head>
+<body>
+    <section class="main">
+        <!-- inicio home -->
+        <section class="home">
+
+        </section>
+        <!-- fim home -->
+        <!-- inicio descricao -->
+        <section class="descricao">
+        
+        </section>
+        <!-- fim descricao -->
+        <!-- inicio vendas -->
+        <section class="vendas">
+
+        </section>
+        <!-- fim vendas -->
+        <!-- inicio app -->
+        <section class="app">
+        
+        </section>
+        <!-- fim app -->
+    </section>
+
+    <!-- scripts -->
+    <script src="assets/components/jquery/dist/jquery.min.js"></script>
+    <script src="assets/components/popper.js/dist/popper.min.js"></script>
+    <script src="assets/components/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
