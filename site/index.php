fd6bbd7edaf302855bd12b1332ac761eaff33088
JulioCVaz <julio.oliveiravaz0@gmail.com>
2018-09-09 14:45:25 -0300

    adicionado navbar

--- site/index.php
+++ site/index.php
@@ -6,13 +6,41 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>som de garagem</title>
     <!-- bootstrap e css -->
-    <link rel="stylesheet" href="assets/components/dist/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="assets/components/bootstrap/dist/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/normalize.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css">
+
+    <!--  -->
 </head>
 <body>
     <section class="main">
         <!-- inicio home -->
         <section class="home">
-
+        <nav class="navbar navbar-expand-lg navbar-home">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">HOME <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">SOBRE</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">PREMIUM</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">INSCREVA-SE</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">ACESSAR</a>
+            </li>
+            </ul>
+        </div>
+        </nav>
         </section>
         <!-- fim home -->
         <!-- inicio descricao -->
