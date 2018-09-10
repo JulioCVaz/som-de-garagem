99e5e482301448e152c2f46f41cf2b8d529897b6
JulioCVaz <julio.oliveiravaz0@gmail.com>
2018-09-09 23:42:04 -0300

    ajustes layout

--- site/index.php
+++ site/index.php
@@ -15,38 +15,51 @@
 <body>
     <!-- inicio home -->
     <section class="home">
-        <!-- navbar -->
-        <nav class="navbar navbar-expand-lg navbar-light navbar-home">
-            <!-- aqui vai ficar a logo -->
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse navbar-wrapper justify-content-end" id="navbarNav">
-            <ul class="navbar-nav navbar-color">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">HOME <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">SOBRE</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">PREMIUM</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">INSCREVA-SE</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">ACESSAR</a>
-            </li>
-            </ul>
-        </div>
-        </nav>
-        <!-- scroll down -->
-        <div class="content-home">
-            <div class="wrapper-content-home">
-                <h4>lorem ipsum dolor sit amet</h4>
-                <a href="#description"><img src="assets/img/scrolldown.gif" alt="gif scroll down"></a>
-                <h4>lorem ipsum dolor sit amet</h4>
+        <div class="container wrapper-all">
+            <div class="row">
+                <div class="col col-md-12">
+                    <!-- navbar -->
+                    <nav class="navbar navbar-expand-lg navbar-light navbar-home">
+                        <!-- aqui vai ficar a logo -->
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse navbar-wrapper justify-content-end" id="navbarNav">
+                        <ul class="navbar-nav navbar-color">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">HOME <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">SOBRE</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">PREMIUM</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">INSCREVA-SE</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">ACESSAR</a>
+                        </li>
+                        </ul>
+                    </div>
+                    </nav>
+                </div>
+            </div>
+            <!-- scroll down -->
+            <div class="wrapper-content">
+                <div class="row">
+                    <div class="col col-md-12">
+                            <div class="content-home">
+                                <div class="wrapper-content-home">
+                                    <h4>lorem ipsum dolor sit amet</h4>
+                                    <a href="#description"><img src="assets/img/scrolldown.gif" alt="gif scroll down"></a>
+                                    <h4>lorem ipsum dolor sit amet</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
@@ -54,25 +67,49 @@
     <!-- inicio descricao -->
     <section id="description" class="descricao">
         <h1>lorem ipsum</h1>
-        <div class="wrapper-circle">
-            <div class="circle-one"></div>
-            <div class="circle-two"></div>
-            <div class="circle-three"></div>
-        </div>
-        <div class="content-descricao">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt ipsum voluptates laboriosam corrupti quia officiis quis minima, vero nesciunt accusantium fugiat suscipit maiores consequatur dolor. Nesciunt sit laboriosam veniam? Quidem!
+        <div class="container wrapper-all">
+            <div class="wrapper-content-circle">
+                <div class="row">
+                    <div class="col col-md-12">
+                        <div class="wrapper-circle">
+                            <div class="circle-one"></div>
+                            <div class="circle-two"></div>
+                            <div class="circle-three"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="wrapper-content-descricao">
+                <div class="row d-flex justify-content-center">
+                    <div class="col col-md-8">
+                        <div class="content-descricao">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt ipsum voluptates laboriosam corrupti quia officiis quis minima, vero nesciunt accusantium fugiat suscipit maiores consequatur dolor. Nesciunt sit laboriosam veniam? Quidem!
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
     <!-- fim descricao -->
     <!-- inicio vendas -->
     <section class="vendas">
-        <div class="wrapper-plan-free">
-            <div class="content-free"></div>
-            <div class="sign-free">EXPERIENTE O FREE</div>
-        </div>
-        <div class="wrapper-plan-premium">
-            <div class="content-premium"></div>
-            <div class="sign-premium">OBTER PREMIUM</div>
+        <div class="container">
+            <div class="row">
+                <div class="col col-md-12">
+                    <div class="wrapper-plan-free">
+                        <div class="content-free"></div>
+                        <div class="sign-free">EXPERIENTE O FREE</div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col col-md-12">
+                    <div class="wrapper-plan-premium">
+                        <div class="content-premium"></div>
+                        <div class="sign-premium">OBTER PREMIUM</div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
     <!-- fim vendas -->
