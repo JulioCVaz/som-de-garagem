5149775120d7e1a17ed7649ee0db78fd9f1ca65e
GusxtaRoberto <grnrodrigues@gmail.com>
2018-10-27 22:39:11 -0300

    Adicionado animações

---
+++ site/assets/js/scripts.js
@@ -0,0 +1,12 @@
+var nav = $('.navbar-toggler');
+
+$(nav).on('click', () => {
+    $('.banner').toggleClass('blur');
+});
+
+jQuery(document).ready(function($) {
+    $(".scroll").click(function(event){
+     event.preventDefault();
+     $('html,body').animate({scrollTop:$(this.hash).offset().top}, 1200);
+    });
+});
