73d7dfb8714866db575457cd67dfc930bd247ee2
JulioCVaz <julio.oliveiravaz0@gmail.com>
2018-11-28 23:45:10 -0200

    criado upload de arquivos no front end

--- plataforma/src/components/UploadMusicas.js
+++ plataforma/src/components/UploadMusicas.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
 import HeaderLayout from './HeaderLayout';
-import classNames from 'classnames';
+import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
+import LinearProgress from '@material-ui/core/LinearProgress';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import api from "../services/api";
@@ -12,23 +14,49 @@ const styles = theme => ({
   paddingWrapper: {
     marginTop: 100
   },
-  // input: {
-  //   display: 'none',
-  // },
+  input: {
+    display: 'none',
+  },
   button: {
     margin: theme.spacing.unit,
+  },
+  root: {
+    flexGrow: 1,
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
   },
 });
 
 class UploadMusicas extends Component{
 
   state = {
-    file: ''
+    file: [],
+    completed: 0,
+    responseComplete: false
   };
 
   handleFile = (e) => {
     this.setState({file:e.target.files[0]});
   }
+
+  componentDidMount() {
+    this.timer = setInterval(this.progress, 500);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    if (completed === 100) {
+      setTimeout(()=>{
+        this.setState({ completed: 0 });
+      }, 2000);
+      this.setState({responseComplete: true});
+    }
+  };
 
   handleSend = (e) => {
     e.preventDefault();
@@ -36,8 +64,11 @@ class UploadMusicas extends Component{
     var n = this.state.file;
     var formdata = new FormData();
     formdata.append("audio", this.state.file, this.state.file.name);
-    api.post('/upload', formdata)
-      .then(
+    api.post('/upload', formdata, {
+      onUploadProgress: progressEvent => {
+        this.setState({completed: Math.round((progressEvent.loaded / progressEvent.total) * 100)})
+      }
+    }).then(
           response => {
             console.log(response);
           }
@@ -47,8 +78,6 @@ class UploadMusicas extends Component{
         }
       )
   }
-
-
   render(){
       const {classes, theme} = this.props;
         return(
@@ -56,28 +85,54 @@ class UploadMusicas extends Component{
             <HeaderLayout/>
             <div className={classes.paddingWrapper}>
               <Grid container justify="center" alignItems="flex-start">
-                <Grid xs={12}>
-                  <Typography variant="h3" gutterBottom>
-                      Upload de Músicas
-                  </Typography>
-                </Grid>
-                <Grid xs={6} direction="row">
-                  <input
-                    accept="audio/*"
-                    className={classes.input}
-                    id="contained-button-file"
-                    multiple
-                    type="file"
-                    onChange={this.handleFile}
-                  />
-                  <label htmlFor="contained-button-file">
-                    <Button variant="contained" component="span" className={classes.button}>
-                      Upload
-                    </Button>
-                  </label>
-                </Grid>
-                <Grid xs={6} direction="row">
-                  <Button onClick={this.handleSend} variant="contained" component="span" className={classes.button}>Enviar</Button>
+                <Grid xs={8}>
+                  <Grid xs={8}>
+                    <Typography variant="h5" gutterBottom>
+                        Upload de Músicas
+                    </Typography>
+                  </Grid>
+                  <hr/>
+                  <Grid xs={8} direction="row">
+                  <div>
+                    <Paper className={classes.root} elevation={1}>
+                      <Typography variant="h5" component="h3">
+                        Título: {this.state.file.name}
+                      </Typography>
+                      <Typography component="p">
+                        Tamanho do arquivo: {this.state.file.size}
+                      </Typography>
+                      <Typography component="p">
+                        Tipo Arquivo: {this.state.file.type}
+                      </Typography>
+                    </Paper>
+                  </div>
+                    <Divider/>
+                    <input
+                      accept="audio/*"
+                      className={classes.input}
+                      id="contained-button-file"
+                      multiple
+                      type="file"
+                      onChange={this.handleFile}
+                    />
+                    <label htmlFor="contained-button-file">
+                      <Button variant="contained" component="span" className={classes.button}>
+                        Upload
+                      </Button>
+                      <Button onClick={this.handleSend} variant="contained" component="span" className={classes.button}>Enviar</Button>
+                    </label>
+                    <br/>
+                    { 
+                      
+                      (this.state.completed > 0) ?
+                        <LinearProgress color="secondary" value={this.state.completed} />
+                        :
+                        (this.state.responseComplete) ? 
+                          'Upload efetuado com sucesso' : ''               
+                    }
+                  </Grid>
+                  <Grid xs={4} direction="row">
+                  </Grid>
                 </Grid>
               </Grid>
             </div>
